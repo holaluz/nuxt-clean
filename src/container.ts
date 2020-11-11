83f7042a986eb0fab555da0c902769b3cb726663
Adrià Fontcuberta <afontcu@gmail.com>
2020-11-11 10:25:44 +0100

    Unify ApiService

--- src/container.ts
+++ src/container.ts
@@ -6,8 +6,15 @@ import { ArticleService as makeArticleService } from '@infrastructure/Article/Ar
 import { createArticle as makeCreateArticle } from '@application/Article/createArticle'
 import { getRecentArticles as makeGetRecentArticles } from '@application/Article/getRecentArticles'
 
+// Dotenv or something
+enum ENDPOINTS {
+  CORE = 'https://jsonplaceholder.typicode.com',
+}
+
+const coreApiService = new ApiService(ENDPOINTS.CORE)
+
 // Infra repositories
-const articleService = makeArticleService({ apiService: new ApiService() })
+const articleService = makeArticleService({ apiService: coreApiService })
 
 // Application
 export const createArticle = makeCreateArticle({ articleService })
--- src/infrastructure/ApiService.ts
+++ src/infrastructure/ApiService.ts
@@ -1,72 +1,94 @@
-import { HttpClient } from '../shared/HttpClient'
-import { HttpError, isHttpError } from '../shared/HttpErrors'
+import axios, {
+  AxiosInstance,
+  AxiosResponse,
+  AxiosRequestConfig,
+  AxiosError,
+} from 'axios'
 import { ApiResult } from '../shared/ApiResponse'
+import { HttpError } from '../shared/HttpError'
 import { err, ok } from '../shared/Result'
-
-export interface Params {
-  [key: string]: unknown
-}
 
 export interface Data {
   [key: string]: unknown
 }
 
-interface IRequestWithoutData {
+type IRequestWithoutData = {
   url: string
-  params?: Params
+  config?: AxiosRequestConfig
 }
 
-interface IRequestWithData {
-  url: string
-  params?: Params
-  data?: Data
-}
+type IRequestWithData = IRequestWithoutData & { data?: Data }
 
 export interface IApiService {
   get<T>(request: IRequestWithoutData): Promise<ApiResult<T>>
   post<T>(request: IRequestWithData): Promise<ApiResult<T>>
 }
 
-// We'll need to add support for additional config
-export class ApiService extends HttpClient implements IApiService {
-  public constructor() {
-    // This should come from a dotenv
-    super('https://jsonplaceholder.typicode.com')
+export class ApiService implements IApiService {
+  protected readonly axiosService: AxiosInstance
+
+  constructor(baseUrl: string) {
+    this.axiosService = axios.create({
+      baseURL: baseUrl,
+      headers: { 'Content-Type': 'application/json' },
+    })
+
+    this._initializeRequestInterceptor()
+    this._initializeResponseInterceptor()
   }
 
-  public async get<T>(request: IRequestWithoutData): Promise<ApiResult<T>> {
+  public async get<T>({
+    url,
+    config,
+  }: IRequestWithoutData): Promise<ApiResult<T>> {
     try {
-      const response = await this.httpService.request<T>({
-        method: 'GET',
-        ...request,
-      })
-
+      const response = await this.axiosService.get<T>(url, config)
       return ok(response.data)
     } catch (error) {
-      return err(this.handleError(error))
+      return err(error)
     }
   }
 
-  public async post<T>(request: IRequestWithData): Promise<ApiResult<T>> {
+  public async post<T>({
+    url,
+    data,
+    config,
+  }: IRequestWithData): Promise<ApiResult<T>> {
     try {
-      const response = await this.httpService.request<T>({
-        method: 'POST',
-        ...request,
-      })
-
+      const response = await this.axiosService.post(url, data, config)
+      throw new Error('whatsaaaaaaaaaa')
       return ok(response.data)
     } catch (error) {
-      return err(this.handleError(error))
+      return err(error)
     }
   }
 
-  private handleError(error: HttpError | Error) {
-    if (isHttpError(error)) {
-      return error
+  private _initializeRequestInterceptor = () => {
+    this.axiosService.interceptors.request.use(
+      this._handleRequest,
+      this._handleError
+    )
+  }
+
+  private _initializeResponseInterceptor = () => {
+    this.axiosService.interceptors.response.use(
+      (response: AxiosResponse) => response,
+      this._handleError
+    )
+  }
+
+  private _handleRequest = (config: AxiosRequestConfig) => {
+    // get this from a cookie, or whatever
+    config.headers.Authorization = 'Bearer ...'
+
+    return config
+  }
+
+  private _handleError = (error: AxiosError): HttpError => {
+    if (error.response) {
+      return HttpError.fromStatus(error.response.status, error.message)
     }
 
-    // This could happen if the request was made but there was no response,
-    // or due to any not-API related error
     throw error
   }
 }
