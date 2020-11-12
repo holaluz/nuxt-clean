1cc5b41c40f6d55ae2d170fe9e88b5da32b4402a
Adrià Fontcuberta <afontcu@gmail.com>
2020-11-12 13:20:31 +0100

    Add docs on VSC tooltip

--- src/shared/Result.ts
+++ src/shared/Result.ts
@@ -21,18 +21,25 @@ class Ok<T, E> {
     return false
   }
 
+  /**
+   * Maps a Result<T, E> to Result<A, E> by applying a function to a contained
+   * Ok value, leaving an Err value untouched.
+   */
   map<A>(f: (t: T) => A): Result<A, E> {
     return ok(f(this.value))
   }
 
+  /**
+   * Maps a Result<T, E> to Result<T, U> by applying a function to a contained
+   * Err value, leaving an Ok value untouched.
+   */
   mapErr<U>(_f: (e: E) => U): Result<T, U> {
     return ok(this.value)
   }
 
   /**
-   * fold() is like chaining map and mapErr, with the distinction that with
-   * fold() both functions must have the same return type, and that it forces
-   * handling of error.
+   * fold() is like chaining map and mapErr, with the distinction that both
+   * functions must have the same return type, and that it forces error handling.
    */
   fold<A>(ok: (t: T) => A, _err: (e: E) => A): A {
     return ok(this.value)
@@ -52,18 +59,25 @@ class Err<T, E> {
     return true
   }
 
+  /**
+   * Maps a Result<T, E> to Result<A, E> by applying a function to a contained
+   * Ok value, leaving an Err value untouched.
+   */
   map<A>(_f: (t: T) => A): Result<A, E> {
     return err(this.error)
   }
 
+  /**
+   * Maps a Result<T, E> to Result<T, U> by applying a function to a contained
+   * Err value, leaving an Ok value untouched.
+   */
   mapErr<U>(f: (e: E) => U): Result<T, U> {
     return err(f(this.error))
   }
 
   /**
-   * fold() is like chaining map and mapErr, with the distinction that with
-   * fold() both functions must have the same return type, and that it forces
-   * handling of error.
+   * fold() is like chaining map and mapErr, with the distinction that both
+   * functions must have the same return type, and that it forces error handling.
    */
   fold<A>(_ok: (t: T) => A, err: (e: E) => A): A {
     return err(this.error)
