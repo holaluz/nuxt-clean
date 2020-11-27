dc07b2908162ccbc76de647ad50b06656d80d2c5
Adrià Fontcuberta <afontcu@gmail.com>
2020-11-27 12:39:45 +0100

    Add ParseError as a valid return type for API requests

--- src/shared/http/HttpResult.ts
+++ src/shared/http/HttpResult.ts
@@ -1,4 +1,5 @@
 import { AsyncResult } from '@@/src/shared/Result'
 import { HttpError } from '@@/src/shared/http/HttpError'
+import { ParseError } from './ParseError'
 
-export type HttpResult<T> = AsyncResult<T, HttpError | Error>
+export type HttpResult<T> = AsyncResult<T, HttpError | ParseError>
