ade4c28191f5c427ee08310f5306333394efd178
Adrià Fontcuberta <afontcu@gmail.com>
2020-11-30 18:27:38 +0100

    Remove log

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,14 +2,10 @@
 const merge = require('lodash.merge')
 const lintStagedConfig = require('@holaluz/npm-scripts').lintStaged
 
-const a = merge(lintStagedConfig, {
+module.exports = merge(lintStagedConfig, {
   '*.{js,ts,vue}': [
     'prettier --write',
     'eslint',
     'npm run test -- --findRelatedTests',
   ],
 })
-
-console.log(a)
-
-module.exports = a
