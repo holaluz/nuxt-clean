1e115932d75b60085a0d673b69d0b6c208eb32e6
Eduard Farre Puig <edu.farrep@gmail.com>
2020-12-02 15:31:43 +0100

    Add rules to vee-validate

--- src/ui/plugins/veeValidate.ts
+++ src/ui/plugins/veeValidate.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue'
-import { ValidationProvider, extend } from 'vee-validate'
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate'
+import { required } from 'vee-validate/dist/rules'
+
 import { createPassword } from '@modules/password/domain'
 import { ParseError } from '@shared/ParseError'
 
@@ -12,5 +14,19 @@ extend('password', (value: string) => {
   return JSON.stringify(result.error.map((e: ParseError) => e.message))
 })
 
+extend('required', {
+  ...required,
+  message: 'required',
+})
+
+extend('confirmed', {
+  params: ['target'],
+  validate(baseValue, { target }) {
+    return baseValue === target
+  },
+  message: 'confirmed',
+})
+
 // Register it globally
-Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('validation-provider', ValidationProvider)
+Vue.component('validation-observer', ValidationObserver)
