c0f2a84c4bea137adf82976eaaa7a43901f3d417
Eduard Farre Puig <edu.farrep@gmail.com>
2020-12-16 11:14:15 +0100

    Change component test

--- src/ui/pages/reset-password/reset-password.spec.ts
+++ src/ui/pages/reset-password/reset-password.spec.ts
@@ -1,20 +1,56 @@
-import { render, fireEvent } from '@ui/utils/nuxt-clean-test-utils'
+import { render, fireEvent, waitFor } from '@ui/utils/nuxt-clean-test-utils'
 import resetPassword from './index.vue'
 
-describe('Reset password page', () => {
-  test('asserts dom properly', () => {
-    const { getByLabelText } = render(resetPassword)
+jest.useFakeTimers()
 
-    expect(getByLabelText(/Password/i)).toBeInTheDocument()
+describe('Reset password page', () => {
+  test.each`
+    inputValue       | errorName      | displayedError
+    ${'1'}           | ${'minLength'} | ${'The field should have at least 8 characters'}
+    ${''}            | ${'required'}  | ${'The field is required'}
+    ${'12345678910'} | ${'maxLength'} | ${'The field cannot have more than 10 characters'}
+  `(
+    `displays $errorName error when it should`,
+    async ({ inputValue, displayedError }) => {
+      const { getByLabelText, findByText, getByText } = render(resetPassword)
+
+      await fireEvent.update(getByLabelText('Password'), inputValue)
+
+      expect(await findByText(displayedError)).toBeInTheDocument()
+      // expect(getByText(/Reset Password/i)).toBeDisabled()
+    }
+  )
+
+  test.only('does not allow to submit form if empty', async () => {
+    const { debug, getByText } = render(resetPassword)
+
+    debug()
+
+    jest.runAllTimers()
+
+    await waitFor(() => expect(getByText(/Reset Password/i)).toBeDisabled())
   })
 
-  test('displays password errors when it should', async () => {
-    const { getByLabelText, findByText } = render(resetPassword)
+  test('displays confirmed error when it should', async () => {
+    const { getByLabelText, findByText, debug, getByText } = render(
+      resetPassword
+    )
 
-    await fireEvent.update(getByLabelText(/Password/i), '1')
+    await fireEvent.update(getByLabelText('Repeat password'), '2')
 
     expect(
-      await findByText(/The field should have at least 8 characters/i)
+      await findByText(/Required fields do not match/i)
     ).toBeInTheDocument()
+    debug()
+    expect(getByText(/Reset Password/i)).toBeDisabled()
+  })
+
+  test('submit button is enabled when form is correct', async () => {
+    const { getByLabelText, getByText } = render(resetPassword)
+
+    await fireEvent.update(getByLabelText('Password'), '123455678')
+    await fireEvent.update(getByLabelText('Repeat password'), '123455678')
+
+    expect(getByText(/Reset Password/i)).not.toBeDisabled()
   })
 })
